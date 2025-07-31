9bdd984f1531424cabe8745b5a66565b9d55a12e
Nycolas <nycolas@teste.com>
2025-07-31 15:52:00 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { ImagePerfil } from './styles'
+
+const Avatar = () => (
+  <ImagePerfil src="https://github.com/Nycolas-klentfelt.png" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Title from '../../components/Title'
+import Avatar from '../../components/Avatar'
+import Paragraph from '../../components/Paragraph'
+import { BtnTheme, Description, SidebarContainer } from './styles'
 
 const Sidebar = () => {
   return (
     <>
       <aside>
-        <img src="https://github.com/Nycolas-Klentfelt.png" />
-        <Title fontSize={20}>Nycolas Klentfelt</Title>
+        <SidebarContainer>
+          <Avatar />
+          <Title fontSize={20}>Nycolas Klentfelt</Title>
+          <Paragraph fontSize={16} tipo="secundario">
+            Nycolas-Klentfelt
+          </Paragraph>
+          <Description tipo="principal" fontSize={12}>
+            Engenheiro Front-end
+          </Description>
+          <BtnTheme>Trocar tema</BtnTheme>
+        </SidebarContainer>
       </aside>
     </>
   )
